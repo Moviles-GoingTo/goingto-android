37f095c3c29db5a2ad479a5004ce0e4003b7401b
Marc Oliva <ing.oliva@hotmail.com>
2020-10-01 08:41:54 -0500

    vistas para el home añadidas hasta la revision del detalle de los lugares

--- app/src/main/java/com/example/goingto/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/goingto/ui/home/HomeFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.example.goingto.R
+import com.example.goingto.adapter.BestRatePlaceAdapterRecyclerView
+import com.example.goingto.adapter.UnknowPlaceAdapterRecyclerView
+import com.example.goingto.model.Place
+import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
 
@@ -22,8 +25,27 @@ class HomeFragment : Fragment() {
 
         homeViewModel =
             ViewModelProviders.of(this).get(HomeViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        root
-        return root
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        /*val tips = ArrayList<Tip>()
+        tips.add(Tip("Tip uno"))
+        val reviews = ArrayList<Review>()
+        reviews.add(Review("Revi uno"))*/
+        val listPlaces = ArrayList<Place>()
+        listPlaces.add(Place("Lugar Uno",resources.getString(R.string.lorem_ipsum) ,5.0,"London"))
+        listPlaces.add(Place("Lugar Uno", resources.getString(R.string.lorem_ipsum),5.0,"London"))
+        listPlaces.add(Place("Lugar Uno", resources.getString(R.string.lorem_ipsum),5.0,"London"))
+        listPlaces.add(Place("Lugar Uno",resources.getString(R.string.lorem_ipsum),5.0,"London"))
+        listPlaces.add(Place("Lugar Uno", resources.getString(R.string.lorem_ipsum),5.0,"London"))
+        listPlaces.add(Place("Lugar Uno", resources.getString(R.string.lorem_ipsum),5.0,"London"))
+        val navController = findNavController()
+        val bestAdapter = BestRatePlaceAdapterRecyclerView(listPlaces,navController)
+        val unknowAdapter = UnknowPlaceAdapterRecyclerView(listPlaces,navController)
+        best_places_recyclerview.adapter = bestAdapter
+        unknow_places_recyclerview.adapter = unknowAdapter
+
     }
 }
